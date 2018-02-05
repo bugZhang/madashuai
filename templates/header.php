df09bee2c45292599e13c8e517248e6d72c0efb6
Jerry Zhang <zjk6700@gmail.com>
2018-02-06 00:02:13 +0800

    update nav

--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,16 @@
 <header class="banner">
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+
+    <nav class="nav-primary navbar navbar-expand-lg navbar-light bg-faded">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navbar-nav mr-auto',
+            'container_class'=>'collapse navbar-collapse', 'container_id' => 'navbarSupportedContent',
+            ]);
       endif;
       ?>
     </nav>
